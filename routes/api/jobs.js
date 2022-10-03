93b15aa0b691f5d39f902738d230f545864be72d
adphuong <aprilphuong@gmail.com>
2022-10-03 15:55:29 -0700

    Implement updateJob - Succesfully update job data

--- routes/api/jobs.js
+++ routes/api/jobs.js
@@ -47,4 +47,19 @@ router.delete("/delete/:id", (req, res) => {
     .catch(err => console.log(err))
 })
 
+router.put("/update/:id", (req, res) => {
+    jobSchema.findByIdAndUpdate({_id: req.params.id}, {
+        company: req.body.company,
+        position: req.body.position,
+        job_link: '//' + req.body.job_link,
+        stage: req.body.stage,
+        next_step: req.body.next_step,
+        date_found: req.body.date_found,
+        date_applied: req.body.date_applied,
+        notes: req.body.notes
+    })
+    .then(doc => console.log(doc))
+    .catch(err => console.log(err))
+})
+
 module.exports = router;
