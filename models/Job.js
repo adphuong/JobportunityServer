af040145e9b6ed32a5104598110a392bc2d45249
adphuong <aprilphuong@gmail.com>
2022-10-03 13:44:49 -0700

    Modified Jobs schema

--- models/Job.js
+++ models/Job.js
@@ -2,27 +2,38 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Join = require("joi");      // object schema descriptions to validate JS objects
 
 
 const JobSchema = new Schema({
   company: {
     type: String,
-    required: true
+    required: true,
+    minlength: 1,
+    maxlength: 255,
+    trim: true
   },
   position: {
     type: String,
-    required: true
+    required: true,
+    minlength: 1,
+    maxlength: 255,
+    trim: true
   },
   stage: {
     type: String,
-    required: true
+    required: true,
+    trim: true
   },
   next_step: {
     type: String,
-    required: true
+    required: true,
+    trim: true
   },
   date_applied: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
   },
   notes: {
     type: String
