ced1d2e94854332f2cc01871171afc223185ea96
adphuong <aprilphuong@gmail.com>
2022-10-05 12:03:15 -0700

    Signing up route function to create new user in db and hashing passwords

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require( 'bcryptjs' );               // Hashing password for security
 
 const UserSchema = new Schema ({
 	// name: {
@@ -18,6 +19,25 @@ const UserSchema = new Schema ({
 		required: true
 	},
 })
+
+
+// Static signup method
+UserSchema.statics.signup = async function(email, password) {
+	// Check if email exists in database
+	const exists = await this.findOne({ email })
+
+	if (exists) {
+		throw Error('Email already in use')
+	}
+
+	const salt = await bcrypt.genSalt(10)
+	const hash = await bcrypt.hash(password, salt)
+
+	const user = await this.create({ email, password: hash})
+
+	return user
+}
+
 
 // mongoose.model("User", UserSchema);
 module.exports = mongoose.model('User', UserSchema)
--- routes/api/users.js
+++ routes/api/users.js
@@ -11,8 +11,17 @@ router.get('/', (req, res) => {
 
 
 // Signup route
-router.post('/signup', (req, res) => {
-    res.json({msg: 'Signup user'})
+router.post('/signup', async (req, res) => {
+    const {email, password} = req.body
+
+    try {
+        const user = await User.signup(email, password)
+
+        res.status(200).json({email, user})
+    } catch(error) {
+        res.status(400).json({error: error.message})
+    }
+
 })
 
 // router.post('/signup', (req, res) => {
